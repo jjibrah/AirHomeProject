96e87bff6e0d2eb5bb33f37db46bc09bbcdf69b8
jjibrah <ibrabdi109@gmail.com>
2023-05-03 10:40:29 +0300

    added item adapter

--- app/src/main/java/com/Abdifatah/airhome/HomeActivity.kt
+++ app/src/main/java/com/Abdifatah/airhome/HomeActivity.kt
@@ -1,11 +1,11 @@
 package com.Abdifatah.airhome
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-    }
+      }
 }
--- app/src/main/java/com/Abdifatah/airhome/MainActivity.kt
+++ app/src/main/java/com/Abdifatah/airhome/MainActivity.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnlogin:Button
     lateinit var btngetstarted:Button
+    lateinit var mAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnlogin = findViewById(R.id.mBtnLoginHome)
+        mAuth = FirebaseAuth.getInstance()
+        if (mAuth.currentUser != null){
+            startActivity(Intent(this,HomeActivity::class.java))
+        }
         btnlogin.setOnClickListener {
             var intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
