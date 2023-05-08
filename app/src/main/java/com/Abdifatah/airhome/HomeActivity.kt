0c2b966215071fcd621c97bc0b2ea564f6394b41
jjibrah <ibrabdi109@gmail.com>
2023-05-08 10:54:18 +0300

    working item adapter

--- app/src/main/java/com/Abdifatah/airhome/HomeActivity.kt
+++ app/src/main/java/com/Abdifatah/airhome/HomeActivity.kt
@@ -9,21 +9,32 @@ class HomeActivity : AppCompatActivity() {
   private lateinit var recyclerView: RecyclerView
   private lateinit var homeList: ArrayList<Homes>
   private lateinit var homeAdapter: HomeAdapter
+
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_home)
 
 
     recyclerView = findViewById(R.id.houses)
-    recyclerView.setHasFixedSize(true)
+    recyclerView.setHasFixedSize(false)
     recyclerView.layoutManager = LinearLayoutManager(this)
+
+    homeList = ArrayList()
 
     homeList.add(Homes(R.drawable.pic1, "Mombasa", "4000"))
     homeList.add(Homes(R.drawable.pic2, "Kisumu", "3800"))
     homeList.add(Homes(R.drawable.pic_new_1, "Malindi", "5000"))
     homeList.add(Homes(R.drawable.pic_new_2, "Watamu", "4500"))
+    homeList.add(Homes(R.drawable.pic1, "Mombasa", "4000"))
+    homeList.add(Homes(R.drawable.pic2, "Kisumu", "3800"))
+    homeList.add(Homes(R.drawable.pic_new_1, "Malindi", "5000"))
+    homeList.add(Homes(R.drawable.pic_new_2, "Watamu", "4500"))
+    homeList.add(Homes(R.drawable.pic1, "Mombasa", "4000"))
+    homeList.add(Homes(R.drawable.pic2, "Kisumu", "3800"))
+    homeList.add(Homes(R.drawable.pic_new_1, "Malindi", "5000"))
+    homeList.add(Homes(R.drawable.pic_new_2, "Watamu", "4500"))
 
-    homeList = ArrayList()
     homeAdapter = HomeAdapter(homeList)
     recyclerView.adapter = homeAdapter
   }
