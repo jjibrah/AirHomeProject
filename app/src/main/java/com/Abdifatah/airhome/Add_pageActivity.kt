39868b79749a24b15c130ec0d2f71ac109b059a0
jjibrah <ibrabdi109@gmail.com>
2023-05-11 09:01:45 +0300

    Ordered id for the items

--- app/src/main/java/com/Abdifatah/airhome/Add_pageActivity.kt
+++ app/src/main/java/com/Abdifatah/airhome/Add_pageActivity.kt
@@ -1,11 +1,37 @@
 package com.Abdifatah.airhome
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 
 class Add_pageActivity : AppCompatActivity() {
+    lateinit var mHouse : ImageButton
+    lateinit var mLocation : EditText
+    lateinit var mHouseDetails : EditText
+    lateinit var mPrice : EditText
+    lateinit var mEmail : EditText
+    lateinit var mPost : Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_page)
+        mHouse = findViewById(R.id.house_image)
+        mLocation = findViewById(R.id.house_location)
+        mHouseDetails = findViewById(R.id.house_details)
+        mPrice = findViewById(R.id.house_price)
+        mEmail = findViewById(R.id.owner_email)
+        mPost = findViewById(R.id.mBtnPost)
+
+
+        //set onclick listeners for each of the items in the page and enable it to post it on firebase DB when post button is pressed
+        mHouse.setOnClickListener {  }
+        mLocation.setOnClickListener {  }
+        mHouseDetails.setOnClickListener {  }
+        mPrice.setOnClickListener {  }
+        mEmail.setOnClickListener {  }
+        mPost.setOnClickListener {  }
     }
 }
--- app/src/main/java/com/Abdifatah/airhome/Homes.kt
+++ app/src/main/java/com/Abdifatah/airhome/Homes.kt
@@ -3,9 +3,10 @@ package com.Abdifatah.airhome
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Homes(var image:Int, var location:String,var price:String) : Parcelable {
+data class Homes(var image:Int, var location:String, var details:String ,var price:String) : Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readInt(),
+        parcel.readString()!!,
         parcel.readString()!!,
         parcel.readString()!!
     ) {
@@ -14,6 +15,7 @@ data class Homes(var image:Int, var location:String,var price:String) : Parcelab
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeInt(image)
         parcel.writeString(location)
+        parcel.writeString(details)
         parcel.writeString(price)
     }
 
